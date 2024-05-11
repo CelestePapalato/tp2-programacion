7e727e0c8154df68a1a7564f5dd93aba8a6b7820
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-05-11 12:05:14 -0300

    Modificadas clases Vida y Damage, ahora en correcto funcionamiento

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    [SerializeField] int damage;
+    [SerializeField] int damagePoints;
+    [SerializeField] float impulse;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(">:)");
+        damage(other.gameObject);
+    }
+
+    private void damage(GameObject gameObject)
     {
         IDamageable enemy;
-        if(TryGetComponent<IDamageable>(out enemy))
+        Rigidbody2D enemyRigidbody;
+        if (!gameObject.TryGetComponent<IDamageable>(out enemy))
         {
-            enemy.Damage(damage);
+            return;
         }
+        enemy.Damage(damagePoints);
+        enemyRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        if (!enemyRigidbody)
+        {
+            enemyRigidbody = gameObject.GetComponentInParent<Rigidbody2D>();
+        }
+        if (enemyRigidbody)
+        {
+            Vector2 position = transform.position;
+            Vector2 impulseVector = enemyRigidbody.position - position;
+            impulseVector.Normalize();
+            enemyRigidbody.AddForce(impulseVector * impulse, ForceMode2D.Impulse);
+        }
+
     }
 }
--- Assets/Scripts/Vida.cs
+++ Assets/Scripts/Vida.cs
@@ -11,16 +11,19 @@ public interface IDamageable
 public class Vida : MonoBehaviour, IDamageable
 {
     [SerializeField] int maxHealth;
+    [SerializeField] float invincibilityTime;
     public UnityEvent<int> Damaged;
     public UnityEvent<int> Healed;
     public UnityEvent Dead;
 
     int health;
-
+    bool invincibility = false;
+    Collider2D col;
 
     private void Awake()
     {
         health = maxHealth;
+        col = GetComponent<Collider2D>();
     }
 
     public void Heal(int healPoints)
@@ -31,11 +34,26 @@ public class Vida : MonoBehaviour, IDamageable
 
     public void Damage(int damagePoints)
     {
+        if(invincibility)
+        {
+            return;
+        }
+        Debug.Log(name + " " + health);
         health = Mathf.Clamp(health - damagePoints, 0, maxHealth);
         Damaged.Invoke(health);
+        invincibilityEnabler();
         if(health <= 0)
         {
             Dead.Invoke();
         }
+    }
+
+    IEnumerator invincibilityEnabler()
+    {
+        invincibility = true;
+        col.enabled = false;
+        yield return new WaitForSeconds(invincibilityTime);
+        invincibility = false;
+        col.enabled = true;
     }
 }
