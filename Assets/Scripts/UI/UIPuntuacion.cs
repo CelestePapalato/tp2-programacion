22cdf1f4c0ec8f13e667740b57b98495b00b6862
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-05-13 11:56:04 -0300

    Añadida suscripción a eventos de GameManager en UIPuntuacion y cambiados métodos de actualización de UI

--- Assets/Scripts/UI/UIPuntuacion.cs
+++ Assets/Scripts/UI/UIPuntuacion.cs
@@ -8,13 +8,25 @@ public class UIPuntuacion : MonoBehaviour
     [SerializeField] TMP_Text UI_Puntuacion;
     [SerializeField] TMP_Text UI_PuntuacionMaxima;
 
+    private void Awake()
+    {
+        GameManager.nuevaPuntuacion += actualizarPuntuacion;
+        GameManager.nuevaPuntuacionMaxima += actualizarPuntuacionMaxima;
+    }
+
     private void Start()
     {
-        actualizarUI();
+        actualizarPuntuacion();
+        actualizarPuntuacionMaxima();
     }
-    private void actualizarUI()
+
+    private void actualizarPuntuacion()
+    {
+        UI_Puntuacion.text = GameManager.Puntuacion.ToString();
+    }
+
+    private void actualizarPuntuacionMaxima()
     {
         UI_PuntuacionMaxima.text = GameManager.PuntuacionMaxima.ToString();
-        UI_Puntuacion.text = GameManager.Puntuacion.ToString();
     }
 }
